454026cd038c0ae895d6eacf5dd6186b64580f55
Ashok Tamang <ashoktamang1108@gmail.com>
2024-11-01 11:39:51 -0400

    feat: exercise and user schema

--- index.js
+++ index.js
@@ -2,6 +2,34 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+let mongoose = require('mongoose')
+
+//set up mongodb
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
+
+let exerciseSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String, 
+    required: true
+  }, 
+  duration: {
+    type: Number, 
+    required: true
+  }, 
+  date: String
+})
+
+let userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  }
+})
+
 
 app.use(cors())
 app.use(express.static('public'))
